04c165916831912ce567880dcb98f666f1bcf58d
mauroprograma10 <programadorcerodiez@gmail.com>
2023-09-11 20:43:50 -0300

    add button to reload grid

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const getGrid = document.getElementById ('grid');
 
 
-
+function random (number) {
+    return Math.floor (Math.random () * (number + 1));
+}
 
 function setSize (size) {
-    for (let i = 0; i < size * size; i++) {
-        const squares = document.createElement ('div');
-        squares.classList.add ('squares');
-        getGrid.appendChild (squares);
         getGrid.style.display = 'grid'
         getGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
         getGrid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-        
+
+    for (let i = 0; i < size * size; i++) {
+        const squares = document.createElement ('div');
+        squares.classList.add ('squares');
+        squares.addEventListener ('mouseout', () => {
+            const rndRol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`
+            squares.style.backgroundColor = rndRol;
+        })
+        getGrid.appendChild (squares);
     }
-
 }
-
 
 setSize (16);
 
