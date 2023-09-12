e6e0e62c638d4d6ef73b132dc06d0d794a3302c0
mauroprograma10 <programadorcerodiez@gmail.com>
2023-09-12 17:07:54 -0300

    add button for reset color

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const getGrid = document.getElementById ('grid');
+const btnRange = document.getElementById ('range');
+const btnClear = document.getElementById ('clear');
+let valorInical = document.getElementById ('valor-inicial');
 
+
+
+btnRange.addEventListener ('click', setSize)
 
 function random (number) {
     return Math.floor (Math.random () * (number + 1));
 }
 
-function setSize (size) {
-        getGrid.style.display = 'grid'
-        getGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-        getGrid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+function setSize () {
+    const gridSize = btnRange.value;
+    valorInical.textContent = gridSize
+    createGrid (gridSize);
+}
+
+function createGrid (size) {
+    getGrid.innerHTML = ''
 
     for (let i = 0; i < size * size; i++) {
-        const squares = document.createElement ('div');
+        const squares = document.createElement ('div')
         squares.classList.add ('squares');
-        squares.addEventListener ('mouseout', () => {
-            const rndRol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`
-            squares.style.backgroundColor = rndRol;
+        squares.addEventListener ('mouseover', () => {
+            rnmCl = `rgb(${random(255)}, ${random(255)}, ${random(255)})`
+            squares.style.backgroundColor = rnmCl
         })
         getGrid.appendChild (squares);
     }
+
+    getGrid.style.display = 'grid'
+    getGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    getGrid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 }
-
-setSize (16);
-
-
